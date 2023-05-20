ec3ccb2edf14e3d2a1ad1c51ca91731dc529cc9b
Rafael <rafael.quelho@gmail.com>
2023-05-20 18:01:38 -0300

    feat: mocking todo list in the UI

--- lib/todo_family_web/live/todos/todos_live.ex
+++ lib/todo_family_web/live/todos/todos_live.ex
@@ -3,14 +3,42 @@ defmodule TodoFamilyWeb.Todos.TodosLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    changeset = build_changeset()
+
     {:ok,
      assign(socket,
        todos: %{
-         Ecto.UUID.generate() => %{text: "test", done: false},
-         Ecto.UUID.generate() => %{text: "test 2", done: true}
+         Ecto.UUID.generate() => %{description: "test", done: false},
+         Ecto.UUID.generate() => %{description: "test 2", done: true}
        },
+       list_uuid: nil,
+       changeset: changeset,
        active_tab: :todos
      )}
+  end
+
+  def handle_event(
+        "save",
+        %{"user" => %{"description" => description}},
+        socket = %{assigns: %{todos: todos}}
+      ) do
+    todos = Map.put(todos, Ecto.UUID.generate(), %{description: description, done: false})
+
+    if is_nil(socket.assigns.list_uuid) do
+      uuid = Ecto.UUID.generate()
+      socket = assign(socket, list_uuid: uuid)
+
+      {:noreply,
+       push_redirect(socket,
+         to: "/todos?uuid=#{uuid}",
+         replace: true
+       )}
+    else
+      {:noreply,
+       assign(socket,
+         todos: todos
+       )}
+    end
   end
 
   @impl true
@@ -24,11 +52,22 @@ defmodule TodoFamilyWeb.Todos.TodosLive do
         Map.put(todo, :done, !todo.done)
       end)
 
-    IO.inspect(todos)
-
     {:noreply,
      assign(socket,
        todos: todos
      )}
+  end
+
+  defp build_changeset(params \\ %{}) do
+    data = %{}
+
+    types = %{
+      description: :string
+    }
+
+    {data, types}
+    |> Ecto.Changeset.cast(params, Map.keys(types))
+    |> Ecto.Changeset.validate_required([:description])
+    |> Ecto.Changeset.validate_length(:description, min: 3, max: 250)
   end
 end
